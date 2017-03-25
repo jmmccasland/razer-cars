a6896bc681972fe0bd655fe057df6735e3a35396
John Michael McCasland <jmmccasland@gmail.com>
2017-03-25 18:02:42 -0500

    feat: add new brands, route: brands/cars/index

--- app/brands/cars/index/route.js
+++ app/brands/cars/index/route.js
@@ -1,12 +1,9 @@
 import Ember from 'ember';
-import config from 'razer-cars/config/environment';
 
 export default Ember.Route.extend({
-  model(params) {
-  // Request part of the URL from 'project.detail' route
-  const id = this.paramsFor('brands.car').id;
+  model() {
+  const id = this.paramsFor('brands.cars').id;
 
-  return fetch(`${config.apiUrl}/brands/${id}`)
-    .then(r => r.json());
-}
+  return this.store.findRecord('brand', id);
+  }
 });
